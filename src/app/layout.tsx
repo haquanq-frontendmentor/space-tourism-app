d55e1d728b3d1007fd3a76d07e8982df00e61a12
Ha Quanq <quangha0120010@gmail.com>
2025-10-26 22:52:30 +0700

    Add font variabes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,25 @@
+import { Header } from "@/components/Header";
 import type { Metadata } from "next";
+import { Barlow, Barlow_Condensed, Bellefair } from "next/font/google";
 import "./globals.css";
+
+const bellefair = Bellefair({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bellefair",
+});
+
+const barlow = Barlow({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-barlow",
+});
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-barlow-condensed",
+});
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Space Tourism Website",
@@ -11,9 +31,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const fonts = [barlowCondensed.variable, barlow.variable, bellefair.variable].join(" ");
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={fonts}>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
