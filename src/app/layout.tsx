f6ae860f0ed3cf4a244ae797877ad73b2dd1bea6
Ha Quanq <quangha0120010@gmail.com>
2025-10-26 13:33:36 +0700

    Change index page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor | Space Tourism Website",
+};
 
 export default function RootLayout({
   children,
