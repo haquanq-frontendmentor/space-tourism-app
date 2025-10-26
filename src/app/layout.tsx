bd7b30ce275e880c90bce1f86ff0c66f68e708f7
Ha Quanq <quangha0120010@gmail.com>
2025-10-26 13:37:07 +0700

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Frontend Mentor | Space Tourism Website",
+  icons: { icon: "/favicon-32x32.png" },
 };
 
 export default function RootLayout({
