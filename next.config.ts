6c3fa54c58f9eff616e9257d017c86b1331fc0e5
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 19:18:27 +0700

    Config Next build path for SSG

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    output: "export",
+    distDir: "dist/",
 };
 
 export default nextConfig;
