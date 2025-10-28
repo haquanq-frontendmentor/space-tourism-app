6d3c96687a85834222ba9500d491e17c10bb9474
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 18:01:28 +0700

    Use custom tailwind merge function

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,6 +1,24 @@
 import clsx, { type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({
+    override: {
+        classGroups: {
+            "font-size": [
+                "text-100",
+                "text-200",
+                "text-300",
+                "text-400",
+                "text-500",
+                "text-600",
+                "text-700",
+                "text-800",
+                "text-900",
+            ],
+        },
+    },
+});
 
 export function cn(...classes: ClassValue[]) {
-    return twMerge(clsx(classes));
+    return customTwMerge(clsx(classes));
 }
