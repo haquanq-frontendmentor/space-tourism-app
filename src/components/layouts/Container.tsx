70a5c3dbd251c315812a7afb07d7b9a1074f84d1
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 08:10:51 +0700

    Adjust Container spacings

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -2,7 +2,10 @@ import { cn } from "@/utils/cn";
 
 export const Container = ({ className, children, ...restProps }: React.ComponentProps<"div">) => {
   return (
-    <div className={cn("mx-auto w-[min(100vw-3rem,69.375rem)]", className)} {...restProps}>
+    <div
+      className={cn("mx-auto w-full max-w-297.5 grow px-[clamp(1.5rem,0.5458rem+4.0712vw,2.5rem)]", className)}
+      {...restProps}
+    >
       {children}
     </div>
   );
