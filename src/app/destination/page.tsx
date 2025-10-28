32d6de35008923e95be3694cd1e02043cfbef243
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 16:22:31 +0700

    Use priority for Destination background Image

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,9 +1,8 @@
 import { Container } from "@/components/layouts/Container";
-import Image from "next/image";
-
 import DesktopBackgroundImage from "@images/destination/background-destination-desktop.jpg";
 import MobileBackgroundImage from "@images/destination/background-destination-mobile.jpg";
 import TabletBackgroundImage from "@images/destination/background-destination-tablet.jpg";
+import Image from "next/image";
 import { DestinationTab } from "./DesintationTab";
 
 export default function Destination() {
@@ -11,9 +10,9 @@ export default function Destination() {
     <main>
       <Container>
         <div className="absolute inset-0 z-10 h-full *:h-full *:w-full *:object-cover">
-          <Image className="md:hidden" src={MobileBackgroundImage} alt="" />
-          <Image className="hidden md:block lg:hidden" src={TabletBackgroundImage} alt="" />
-          <Image className="hidden lg:block" src={DesktopBackgroundImage} alt="" />
+          <Image className="md:hidden" src={MobileBackgroundImage} alt="" priority />
+          <Image className="hidden md:block lg:hidden" src={TabletBackgroundImage} alt="" priority />
+          <Image className="hidden lg:block" src={DesktopBackgroundImage} alt="" priority />
         </div>
         <section className="relative z-20 pt-[clamp(1.5rem,0.0687rem+6.1069vw,3rem)] pb-[clamp(1.5rem,-7.5649rem+38.6768vw,11rem)]">
           <h1 className="text-500 text-white uppercase">
