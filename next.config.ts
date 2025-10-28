177e3d3dd1d288b28daa03b8b04ba3153ac567ad
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 21:16:00 +0700

    Config Next build images optimization to false

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     output: "export",
     distDir: "./dist",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
