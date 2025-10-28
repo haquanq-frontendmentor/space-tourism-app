2052add0c7b65ffdc2576ece6e6ab48e7a802ad2
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 20:25:36 +0700

    Add focus outline for TabButton

--- src/components/Tab/TabButton.tsx
+++ src/components/Tab/TabButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/utils/cn";
 import { useContext } from "react";
 import { TabContext } from "./context";
 
@@ -7,7 +8,7 @@ interface TabButtonProps extends React.ComponentProps<"button"> {
   index: number;
 }
 
-export const TabButton = ({ children, index, ...restProps }: TabButtonProps) => {
+export const TabButton = ({ children, index, className, ...restProps }: TabButtonProps) => {
   const { selectedIndex, setSelectedIndex } = useContext(TabContext);
 
   const handleClick = () => {
@@ -15,7 +16,17 @@ export const TabButton = ({ children, index, ...restProps }: TabButtonProps) => 
   };
 
   return (
-    <button type="button" role="tab" aria-selected={selectedIndex === index} onClick={handleClick} {...restProps}>
+    <button
+      className={cn(
+        "focus-visible:outline-[3px] focus-visible:outline-offset-[3px] focus-visible:outline-white focus-visible:outline-dashed",
+        className,
+      )}
+      type="button"
+      role="tab"
+      aria-selected={selectedIndex === index}
+      onClick={handleClick}
+      {...restProps}
+    >
       {children}
     </button>
   );
