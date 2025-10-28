0933476a5b295b7337449e9bb64c627b896f1bfd
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 13:17:52 +0700

    Add Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,45 @@
+import { Container } from "@/components/layouts/Container";
+import Image from "next/image";
+import Link from "next/link";
+
+import DesktopBackgroundImage from "@images/home/background-home-desktop.jpg";
+import MobileBackgroundImage from "@images/home/background-home-mobile.jpg";
+import TabletBackgroundImage from "@images/home/background-home-tablet.jpg";
+
 export default function Home() {
-  return <></>;
+  return (
+    <main className="flex grow flex-col">
+      <Container className="flex flex-col">
+        <div className="absolute inset-0 z-10 h-full *:h-full *:w-full *:object-cover">
+          <Image className="md:hidden" src={MobileBackgroundImage} alt="" />
+          <Image className="hidden md:block lg:hidden" src={TabletBackgroundImage} alt="" />
+          <Image className="hidden lg:block" src={DesktopBackgroundImage} alt="" />
+        </div>
+        <section className="relative z-20 flex h-full grow py-[clamp(1.5rem,-4.7023rem+26.4631vw,8rem)] lg:items-end">
+          <div className="flex w-full flex-col lg:flex-row lg:justify-between">
+            <div className="flex flex-col items-center gap-6 text-center lg:items-start lg:text-start">
+              <h1 className="font-accent flex flex-col items-center gap-6 uppercase lg:items-start">
+                <span className="text-600 text-blue-300">So, you want to travel to</span>
+                <span className="text-100 text-white">space</span>
+              </h1>
+              <p className="font-primary text-900 max-w-136 justify-self-center text-sm leading-6.75 text-blue-300">
+                Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover
+                kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world
+                experience!
+              </p>
+            </div>
+            <div className="flex items-center justify-center pt-[clamp(2.625rem,9.0983rem+-13.486vw,5.9375rem)] lg:p-0">
+              <Link
+                className="text-400 font-accent relative flex aspect-square w-[clamp(9rem,1.3664rem+32.57vw,17rem)] items-center justify-center rounded-full bg-white uppercase"
+                href="/destination"
+              >
+                <span className="absolute inset-[clamp(3rem,0.6145rem+10.1781vw,5.5rem)] block rounded-full bg-white opacity-25"></span>
+                Explore
+              </Link>
+            </div>
+          </div>
+        </section>
+      </Container>
+    </main>
+  );
 }
